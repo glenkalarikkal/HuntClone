93f337f0e9d23221aa48ecf6dbde6352a74e6ec4
Glen Martin <glen.martin@truecaller.com>
2020-10-12 23:30:13 +0530

    Base IonApp with IonTabs and ReachRouter Base pages setup for each required page

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Home</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </IonPage>
+    );
+};
+
+export default Home;
