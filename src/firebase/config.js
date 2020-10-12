a4ceac60a50354a8fce451895380c842f6ee3595
Glen Martin <glen.martin@truecaller.com>
2020-10-12 23:59:31 +0530

    Add Firebase config Add auth and firestore config create module to encapsulate both

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDdKADY5YOXROCeAcJxlU9DIPioKQs-LI4",
+    authDomain: "huntclone.firebaseapp.com",
+    databaseURL: "https://huntclone.firebaseio.com",
+    projectId: "huntclone",
+    storageBucket: "huntclone.appspot.com",
+    messagingSenderId: "316320501900",
+    appId: "1:316320501900:web:8dd6088242f278469daba9"
+  };
+
+export default firebaseConfig;
